2c17dad74f6e26e810fce2d380c2e158d333f80c
Grant Gould <ggould@alum.mit.edu>
2024-12-02 16:47:57 -0500

    Day 1 complete

--- src/day1.rs
+++ src/day1.rs
@@ -1,8 +1,7 @@
+use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead};
 use std::iter::zip;
 use std::vec::Vec;
-
-use std::fs::File;
 
 fn read_all_records<T>(readable: T) -> Vec<Vec<String>>
 where
@@ -41,6 +40,26 @@ fn sorted_error_sum(records: Vec<(i32, i32)>) -> i32 {
     result
 }
 
+fn similarity_score(records: Vec<(i32, i32)>) -> i32 {
+    let mut result: i32 = 0;
+    let (left, right): (Vec<i32>, Vec<i32>) = records.iter().cloned().unzip();
+    let keys: HashSet<i32> =
+        &HashSet::from_iter(left.iter().cloned()) | &HashSet::from_iter(left.iter().cloned());
+    let mut left_counts: HashMap<i32, i32> = HashMap::new();
+    let mut right_counts: HashMap<i32, i32> = HashMap::new();
+    for l in left {
+        left_counts.insert(l, left_counts.get(&l).unwrap_or(&0) + 1);
+    }
+    for r in right {
+        right_counts.insert(r, right_counts.get(&r).unwrap_or(&0) + 1);
+    }
+    for k in keys {
+        let score: i32 = k * left_counts.get(&k).unwrap_or(&0) * right_counts.get(&k).unwrap_or(&0);
+        result += score;
+    }
+    result
+}
+
 pub fn day1() -> i32 {
     let records = read_all_records(io::stdin().lock());
     let parsed = parse_records(records);
@@ -48,14 +67,22 @@ pub fn day1() -> i32 {
     result
 }
 
+pub fn day1b() -> i32 {
+    let records = read_all_records(io::stdin().lock());
+    let parsed = parse_records(records);
+    let result = similarity_score(parsed);
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use io::BufReader;
+    use std::fs::File;
 
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_example_1() {
         let f = File::open("data/day1_example.txt").unwrap();
         let reader = BufReader::new(f);
         let example_records = read_all_records(reader);
@@ -64,11 +91,29 @@ mod tests {
     }
 
     #[test]
-    fn test_test() {
+    fn test_test_1() {
         let f = File::open("data/day1_test.txt").unwrap();
         let reader = BufReader::new(f);
         let example_records = read_all_records(reader);
         let result = sorted_error_sum(parse_records(example_records));
         assert_eq!(result, 1319616);
+    }
+
+    #[test]
+    fn test_example_1b() {
+        let f = File::open("data/day1_example.txt").unwrap();
+        let reader = BufReader::new(f);
+        let example_records = read_all_records(reader);
+        let result = similarity_score(parse_records(example_records));
+        assert_eq!(result, 31);
+    }
+
+    #[test]
+    fn test_test_1b() {
+        let f = File::open("data/day1_test.txt").unwrap();
+        let reader = BufReader::new(f);
+        let example_records = read_all_records(reader);
+        let result = similarity_score(parse_records(example_records));
+        assert_eq!(result, 27267728);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::env;
+
 pub mod day1;
 
 fn main() {
-    print!("{}", day1::day1());
+    if env::args().len() == 1 {
+        println!("{}", day1::day1b());
+    } else {
+        for arg in env::args().skip(1) {
+            println!(
+                "{}",
+                match arg.as_str() {
+                    "day1" => day1::day1(),
+                    "day1b" => day1::day1b(),
+                    _ => panic!("Skipping unknown test {}", arg),
+                }
+            )
+        }
+    }
 }
