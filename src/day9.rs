9285944f3c2f581c7ee27a30c866b88734828105
Grant Gould <ggould@alum.mit.edu>
2024-12-09 08:30:24 -0500

    WIP day 9

--- src/day9.rs
+++ src/day9.rs
@@ -36,19 +36,19 @@ fn str_to_rle(source: &String) -> RleList {
     result
 }
 
-fn rle_to_map_str(rle: RleList) -> String {
+fn rle_to_map_str(rle: &RleList) -> String {
     let mut result = String::new();
     for RleItem{id, length} in rle {
         match id {
-            Some(i) => { for _ in 0..length { result += &(i % 10).to_string(); } }
-            None => { for _ in 0..length { result += &".".to_string(); } }
+            Some(i) => { for _ in 0..*length { result += &(i % 10).to_string(); } }
+            None => { for _ in 0..*length { result += &".".to_string(); } }
         }
     }
     result
 }
 
 
-fn is_compact(rle: RleList) -> bool {
+fn is_compact(rle: &RleList) -> bool {
     for i in 0..rle.len()-1 {
         let RleItem{id, length} = rle[i];
         if id.is_none() && length > 0 { return false; }
@@ -56,9 +56,48 @@ fn is_compact(rle: RleList) -> bool {
     true
 }
 
+fn index_of_first_empty_item(rle: &RleList) -> usize {
+    rle.iter().position(|item| item.id.is_none()).unwrap()
+}
+
+fn index_of_last_nonempty_item(rle: &RleList) -> usize {
+    let reverse_pos = rle.iter().rev().position(|item| !item.id.is_none()).unwrap();
+    rle.len() - reverse_pos - 1
+}
+
+fn write_block(rle: &mut RleList, item: &RleItem) {
+    let id = item.id;
+    let mut remaining_length = item.length;
+    let mut insert_index = index_of_first_empty_item(rle);
+    while rle[insert_index].length < remaining_length {
+        rle[insert_index].id = id;
+        remaining_length -= rle[insert_index].length;
+        insert_index = index_of_first_empty_item(rle);
+    }
+    if remaining_length == 0 { return; }  // Avoid inserting zero-length blocks.
+    rle[insert_index].length -= remaining_length;
+    rle.insert(insert_index, RleItem{id:id, length:remaining_length});
+}
+
+fn move_last_nonempty_block(rle: &mut RleList) {
+    let index = index_of_last_nonempty_item(rle);
+    println!("Last nonempty index is {}", index);
+    let item: RleItem = {
+        let mut_item: &mut RleItem = rle.get_mut(index).unwrap();
+        mut_item.length = 0;
+        mut_item.clone()
+    };  // Drop mutability
+    write_block(rle, &item);
+}
+
 pub fn day9(source: Option<String>) -> i64 {
     let lines = read_lines(source);
-    println!("{}", rle_to_map_str(str_to_rle(&lines[0])));
+    let mut rle = str_to_rle(&lines[0]);
+    while !is_compact(&rle) {
+        println!("{}", rle_to_map_str(&rle));
+        move_last_nonempty_block(&mut rle);
+    }
+    println!("{}", rle_to_map_str(&rle));
     0
 }
 
