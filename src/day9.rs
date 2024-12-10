3e7ced26c971d5c4c511de295e05bbb5a7467422
Grant Gould <ggould@alum.mit.edu>
2024-12-09 22:19:40 -0500

    WIP failing 9b

--- src/day9.rs
+++ src/day9.rs
@@ -60,10 +60,14 @@ fn rle_to_map_str(rle: &RleList) -> String {
 }
 
 fn is_compact(rle: &RleList) -> bool {
-    for i in 0..rle.len() - 1 {
-        let RleItem { id, length } = rle[i];
-        if id.is_none() && length > 0 {
-            return false;
+    let mut found_empty = false;
+    for item in rle {
+        if item.length > 0 {
+            if item.id.is_none() && item.length > 0 {
+                found_empty = true;
+            } else {
+                if found_empty { return false; }
+            }
         }
     }
     true
@@ -77,7 +81,7 @@ fn index_of_last_nonempty_item(rle: &RleList) -> usize {
     let reverse_pos = rle
         .iter()
         .rev()
-        .position(|item| !item.id.is_none())
+        .position(|item| !item.id.is_none() && item.length > 0)
         .unwrap();
     rle.len() - reverse_pos - 1
 }
@@ -106,28 +110,60 @@ fn write_block(rle: &mut RleList, item: &RleItem) {
 
 fn move_last_nonempty_block(rle: &mut RleList) {
     let index = index_of_last_nonempty_item(rle);
-    println!("Last nonempty index is {}", index);
-    let item: RleItem = {
+    let item_copy = rle[index].clone();
+    {  // Mutate the item within the list.
         let mut_item: &mut RleItem = rle.get_mut(index).unwrap();
-        mut_item.length = 0;
-        mut_item.clone()
-    }; // Drop mutability
-    write_block(rle, &item);
+        mut_item.id = None;
+    };
+    write_block(rle, &item_copy);
+}
+
+fn try_to_move(rle: &mut RleList, id_to_move: Id) {
+    let item_to_move: &mut RleItem = rle.iter_mut().find(|i| i.id == Some(id_to_move)).unwrap();
+    println!("Item to move: {:?}", item_to_move);
+}
+
+fn score(rle: &RleList) -> i64 {
+    let mut result: i64 = 0;
+    let mut pos: Scalar = 0;
+    for item in rle {
+        for _ in 0..item.length {
+            result += i64::from(item.id.unwrap_or_default() * pos);
+            pos += 1;
+        }
+    }
+    result
 }
 
 pub fn day9(source: Option<String>) -> i64 {
     let lines = read_lines(source);
     let mut rle = str_to_rle(&lines[0]);
+    let mut counter = 0;
     while !is_compact(&rle) {
-        println!("{}", rle_to_map_str(&rle));
+        counter += 1;
+        if counter % 50 == 0 {
+            println!("{}", rle_to_map_str(&rle));
+        }
         move_last_nonempty_block(&mut rle);
     }
     println!("{}", rle_to_map_str(&rle));
-    0
+    score(&rle)
 }
 
 pub fn day9b(source: Option<String>) -> i64 {
-    day9(source)
+    let lines = read_lines(source);
+    let mut rle = str_to_rle(&lines[0]);
+    let mut counter = 0;
+    let ids: Vec<Id> = rle.iter().filter_map(|i| i.id).rev().collect();
+    for id in ids {
+        counter += 1;
+        if counter % 50 == 0 {
+            println!("{}", rle_to_map_str(&rle));
+        }
+        try_to_move(&mut rle, id)
+    }
+    println!("{}", rle_to_map_str(&rle));
+    score(&rle)
 }
 
 #[cfg(test)]
@@ -135,21 +171,20 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore = "TODO(ggould) Not yet done."]
     fn test_example() {
-        assert_eq!(day9(Some("data/day9_example.txt".to_string())), 14);
+        assert_eq!(day9(Some("data/day9_example.txt".to_string())), 1928);
     }
 
     #[test]
     #[ignore = "requires input not in repository"]
     fn test_test() {
-        assert_eq!(day9(Some("inputs/day9_test.txt".to_string())), 344);
+        assert_eq!(day9(Some("inputs/day9_test.txt".to_string())), 6331212425418);
     }
 
     #[test]
-    #[ignore = "TODO(ggould) Not yet done."]
+    #[ignore = "not implemented"]
     fn test_example_b() {
-        assert_eq!(day9b(Some("data/day9_example.txt".to_string())), 34);
+        assert_eq!(day9b(Some("data/day9_example.txt".to_string())), 2858);
     }
 
     #[test]
